067f81fc7a8a49501c1647394f46b229ff0dbfd5
romulosilvestre <romulo.cesardf@hotmail.com>
2024-05-17 21:52:33 -0300

    tecnico e nivel ok

--- app/models/alpha/tecnico_model.py
+++ app/models/alpha/tecnico_model.py
@@ -1,4 +1,5 @@
 from app import db #SQLAlchemy - Migrate:Migrar Classe para Tabela
+from sqlalchemy import Text
 
 class Tecnico(db.Model):
     __tablename__ = "tecnico"
@@ -6,4 +7,5 @@ class Tecnico(db.Model):
     id = db.Column(db.Integer,primary_key=True,autoincrement=True)
     nome  = db.Column(db.String(200))
     email  = db.Column(db.String(200))
+    mini_bio = db.Column(Text, nullable=False)
     fk_nivel_id= db.Column(db.Integer,db.ForeignKey('nivel.id'))
--- app/views/tecnico_view.py
+++ app/views/tecnico_view.py
@@ -2,10 +2,19 @@ from flask import render_template, redirect, url_for
 from app import app, db
 from app.forms.alpha import tecnico_form
 from app.models.alpha import tecnico_model
+from app.models.alpha import nivel_model
 
 @app.route("/cadtecnico", methods=["POST", "GET"])
 def cadastrar_tecnico_ok():
     form = tecnico_form.TecnicoForm()
+    
+    # Carregar os níveis do banco de dados
+    niveis = nivel_model.Nivel.query.all()
+    # Formatar os níveis para o formato necessário para o campo de seleção
+    nivel_choices = [(nivel.id, nivel.nome) for nivel in niveis]
+    # Adicionar os níveis ao campo de seleção
+    form.nivel_id.choices = nivel_choices
+    
     if form.validate_on_submit():
         nome = form.nome.data
         nivel_id = form.nivel_id.data
@@ -18,12 +27,4 @@ def cadastrar_tecnico_ok():
             print(f"Erro ao cadastrar técnico:{e}")
           
     return render_template("tecnico/form_tecnico.html", form=form)
-
-"""
-@app.route("/listatecnicos")
-def listar_tecnicos():
-    tecnicos = Tecnico.query.all()
-    return render_template("tecnico/lista_tecnico.html", tecnicos=tecnicos)
-
-"""
 
